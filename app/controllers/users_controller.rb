50b4834cc09b70ab172ced2f72107c1db0e59cba
tiyk-a <milejanaja@gmail.com>
2019-05-26 10:07:23 +0000

    [Update]User views and controller was added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :set_admin, only: [:index]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = current_user
+    @keep = Keep.new
+    gon.keeps = []
+    keeps = Keep.where(user_id: current_user)
+    keeps.each do |k|
+      gon.keeps.push(k.concert.venue)
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def add_admin
+    @user = User.find(params[:id])
+    @user.update_column(:admin, true)
+    redirect_to users_path
+  end
+
+  def destroy_admin
+    @user = User.find(params[:id])
+    @user.update_column(:admin, false)
+    redirect_to users_path
+  end
+
+  def about
+    gon.venues = Venue.all
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.fetch(:user, :admin, {})
+  end
+end
