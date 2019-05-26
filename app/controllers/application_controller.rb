e07817c8b24559cbd0bcea76835c141fc0f8868e
tiyk-a <milejanaja@gmail.com>
2019-05-26 10:29:02 +0000

    [Update]Admin column and admin? method was added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :info, :warning, :danger
+  helper_method :admin_user?
+
+  def after_sign_in_path_for(resource)
+    concerts_path
+  end
+
+  def after_sign_out_path_for(resource)
+    concerts_path
+  end
+
+  def admin_user?
+    user_signed_in? && current_user.admin == true
+  end
+
+  def set_admin
+    if user_signed_in? && current_user.admin == true
+    else
+      redirect_to concerts_path
+    end
+  end
 end
