760136cdf089b6b4f64af6b110401b2ed8fe8e93
tiyk-a <milejanaja@gmail.com>
2019-05-26 09:40:54 +0000

    [Update]User table was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :watch_artists
   resources :venues
   resources :performers
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_092044) do
+ActiveRecord::Schema.define(version: 2019_05_26_093319) do
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name"
@@ -45,6 +45,37 @@ ActiveRecord::Schema.define(version: 2019_05_26_092044) do
 
   create_table "performers", force: :cascade do |t|
     t.integer "concert_id"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "country"
+    t.string "city"
+    t.string "address"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "watch_artists", force: :cascade do |t|
+    t.integer "user_id"
     t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
