5d5d07ddc5693590688f2d9cf157a87c3ec45380
tiyk-a <milejanaja@gmail.com>
2019-05-30 03:18:45 +0000

    [heroku]adjusted

--- db/migrate/20190526092044_create_performers.rb
+++ db/migrate/20190526092044_create_performers.rb
@@ -1,6 +1,6 @@
 class CreatePerformers < ActiveRecord::Migration[5.2]
   def change
-    create_table :performers, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
+    create_table :performers do |t|
       t.integer :concert_id
       t.integer :artist_id
 
