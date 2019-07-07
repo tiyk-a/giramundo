2f29fa50172e843fc376275873bcf039a29707e9
chiara <milejanaja@gmail.com>
2019-07-07 13:54:02 +0000

    :traffic_light:After loggedin route changed to artists_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :gg_key
 
   def after_sign_in_path_for(resource)
-    concerts_path
+    artists_path
   end
 
   def after_sign_out_path_for(resource)
